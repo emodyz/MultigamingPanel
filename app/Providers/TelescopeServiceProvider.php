c6ed8449b9ee972c1435edda12acc483f0a7981b
root <root@srv01.flashmodz.fr>
2020-06-01 01:34:11 +0200

    ADD THEME

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,7 +21,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if (!$this->app->environment('production')) {
+            if ($this->app->environment('local')) {
                 return true;
             }
 
