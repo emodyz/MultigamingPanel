cd8d4133453fe01b33b2af4eef4b4976bfff84ed
Leo Hubert <leo.hubert@epitech.eu>
2020-06-02 22:49:13 +0200

    Clean

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Request;
-
 class LocaleController extends Controller
 {
-    public function update(Request $request, $locale)
+    public function update($locale)
     {
         auth()->user()->update([
             'locale' => $locale
