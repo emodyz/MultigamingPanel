41d953d92cfb7a856da67b23daeb4ce9aa6ef683
Leo Hubert <leo.hubert@epitech.eu>
2020-11-01 11:12:28 +0100

    Add seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        tap(User::factory()->create([
+           'email' => 'root@root.com',
+           'password' => Hash::make('root')
+        ]), function (User $user) {
+            $user->ownedTeams()->save(Team::forceCreate([
+                'user_id' => $user->id,
+                'name' => explode(' ', $user->name, 2)[0]."'s Team",
+                'personal_team' => true,
+            ]));
+        });
+    }
+}
