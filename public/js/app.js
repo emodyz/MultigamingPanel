af3bbf6f7d1bfc727510f10f447506c540f7e7fc
Saber <contact@wirk.fr>
2020-11-02 15:47:49 +0100

    \ #9 compiling assets

--- public/js/app.js
+++ public/js/app.js
@@ -44229,8 +44229,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/hubert_i/Launcher/multigaming/MultigamingPanel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/hubert_i/Launcher/multigaming/MultigamingPanel/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/wirk/Documents/work.nosync/Emodyz/MultigamingPanel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/wirk/Documents/work.nosync/Emodyz/MultigamingPanel/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
