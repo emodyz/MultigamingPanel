e57e2c20e7a7ad6fae2e05ece822ae93c03957e1
Leo Hubert <leo.hubert@epitech.eu>
2021-01-19 23:39:18 +0100

    Feature #35 Add more test

--- app/Jobs/ProcessServersStatuses.php
+++ app/Jobs/ProcessServersStatuses.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\Models\Server;
 use App\Services\Games\GamesStatusService;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -12,18 +13,22 @@ use Illuminate\Queue\SerializesModels;
 
 class ProcessServersStatuses implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+  use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     * @throws \Exception
-     */
-    public function handle()
-    {
-        $servers = Server::all();
+  /**
+   * Execute the job.
+   *
+   * @return void
+   * @throws Exception
+   */
+  public function handle()
+  {
+    $gameStatusService = app()->make(GamesStatusService::class);
 
-        $servers->each(fn(Server $server) => GamesStatusService::fetchStatus($server));
-    }
+    Server::chunk(20, function ($servers) use ($gameStatusService) {
+      foreach ($servers as $server) {
+        $gameStatusService->fetchServerStatus($server);
+      }
+    });
+  }
 }
