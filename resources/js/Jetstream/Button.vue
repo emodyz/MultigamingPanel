ebe6e5f1aaf4f2a3cdb9069025360a1943c7483f
Saber Nouira <contact@wirk.fr>
2021-02-03 14:56:57 +0100

    dark modpacks

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -7,7 +7,7 @@
     border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900
     focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-400 transition ease-in-out duration-150
     dark:text-gray-100 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:active:bg-indigo-500 dark:focus:border-indigo-400
-    dark:focus:ring-indigo-400"
+    dark:focus:ring-indigo-400 dark:disabled:bg-gray-700"
   >
     <slot />
   </button>
