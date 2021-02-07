e027735b06462113ae94bd041af28e42f9a657d0
Leo Hubert <leo.hubert@epitech.eu>
2021-02-08 00:33:46 +0100

    Feature #70 - Support 2FA for api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,9 +7,11 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Contracts\TwoFactorAuthenticationProvider;
 
 class LoginController extends Controller
 {
@@ -21,24 +23,69 @@ class LoginController extends Controller
     public function __invoke(Request $request)
     {
         $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-            'device_name' => 'required',
+          'email' => 'required|email',
+          'password' => 'required',
+          'device_name' => 'required'
         ]);
 
         $user = User::where('email', $request->email)->first();
 
-        if (! $user || ! Hash::check($request->password, $user->password)) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
+              'email' => ['The provided credentials are incorrect.'],
             ]);
+        }
+
+        if ($user->two_factor_secret) {
+            if (!$request->has(['two_factor_code', 'two_factor_recovery_code'])) {
+                return response()->json([
+                  'message' => 'Two Factor authentication required.',
+                ], Response::HTTP_I_AM_A_TEAPOT);
+            }
+
+            $recoveryCode = $request->get('two_factor_recovery_code');
+            $code = $request->get('two_factor_code');
+
+            if ($recoveryCode && $this->isValidRecoveryCode($user, $request->get('two_factor_recovery_code'))) {
+                $user->replaceRecoveryCode($recoveryCode);
+            } else if (!$code || !$this->isValidCode($user, $code)) {
+                throw ValidationException::withMessages([
+                  'two_factor' => ['The provided two factor are incorrect.'],
+                ]);
+            }
         }
 
         $token = $user->createToken($request->device_name);
 
         return response()->json([
-            'access_token' => Str::after($token->plainTextToken, '|'),
-            'user' => UserResource::make($user)
+          'access_token' => Str::after($token->plainTextToken, '|'),
+          'user' => UserResource::make($user)
         ]);
+    }
+
+    /**
+     * Check if the recovery code is valid.
+     *
+     * @param  User  $user
+     * @param  string  $recoveryCode
+     * @return string|null
+     */
+    public function isValidRecoveryCode(User $user, string $recoveryCode): ?string
+    {
+        return collect($user->recoveryCodes())
+          ->first(fn($code) => hash_equals($recoveryCode, $code) ? $code : null);
+    }
+
+    /**
+     * Check if the two factor code is valid.
+     *
+     * @param  User  $user
+     * @param  string  $code
+     * @return bool
+     */
+    public function isValidCode(User $user, string $code): bool
+    {
+        return app(TwoFactorAuthenticationProvider::class)
+          ->verify(decrypt($user->two_factor_secret), $code);
     }
 }
--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -3,9 +3,9 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -49,5 +49,22 @@ class LoginTest extends TestCase
             'password' => 'WrongPassword',
             'device_name' => 'UnitTests'
         ])->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @test
+     */
+    public function if_2FA_is_enabled_i_am_a_teapot_status_must_be_returned_if_no_code_was_provided()
+    {
+        $user = User::factory()->create([
+          'password' => Hash::make(self::USER_PASSWORD),
+          'two_factor_secret' => Str::random(10)
+        ]);
+
+        $this->post('/api/auth/login', [
+          'email' => $user->email,
+          'password' => self::USER_PASSWORD,
+          'device_name' => 'UnitTests'
+        ])->assertStatus(Response::HTTP_I_AM_A_TEAPOT);
     }
 }
