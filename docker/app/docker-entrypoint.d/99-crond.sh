7823dbbf7047fa05afb62ce13c2ade2943cb0849
Saber Nouira <contact@wirk.fr>
2021-05-13 21:52:50 +0200

    fighting crond | round 3

--- docker/app/docker-entrypoint.d/99-crond.sh
+++ docker/app/docker-entrypoint.d/99-crond.sh
@@ -1,19 +1,3 @@
 #!/bin/sh -e
-
-case $1 in
-
-  web_server)
-    YOUR WEB SERVER COMMAND
-  ;;
-
-  cron_jobs)
-     exec crond -f
-  ;;
-
-  *)
-    exec "$@"
-  ;;
-
-esac
-
+  exec crond -b -L /dev/stdout
 exit 0
