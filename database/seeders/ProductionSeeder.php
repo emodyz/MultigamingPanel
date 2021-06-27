493e562b6a5efe96c918b333db5fcb22707475f0
Saber Nouira <contact@wirk.fr>
2021-06-27 09:46:28 +0200

    grpc ext

---
+++ database/seeders/ProductionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Jobs\ProcessServersStatuses;
+use Illuminate\Database\Seeder;
+
+//TODO: Implement this!
+class ProductionSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
